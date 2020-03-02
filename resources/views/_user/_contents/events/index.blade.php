5375c7ddf4a29d03d2ec303fa5124e0877301612
Syifa Nurzain <elzainsyifa@gmail.com>
2020-03-02 08:58:28 +0700

    layout_change_bg

--- resources/views/_user/_contents/events/index.blade.php
+++ resources/views/_user/_contents/events/index.blade.php
@@ -8,35 +8,12 @@
 	      	<div class="overlay"></div>
 	        <div class="container-fluid p-0">
 	          <div class="row d-md-flex no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
-	          	<img class="one-third order-md-last img-fluid" src="{{ URL::asset('user/images/bg_1.png') }}" alt="">
+	          	
 		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
 		          	<div class="text">
-		          		<span class="subheading">#New Arrival</span>
-		          		<div class="horizontal">
-				            <h1 class="mb-4 mt-3">Shoes Collection 2019</h1>
-				            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
-				            
-				            <p><a href="#" class="btn-custom">Discover Now</a></p>
-				          </div>
-		            </div>
-		          </div>
-	        	</div>
-	        </div>
-	      </div>
-
-	      <div class="slider-item js-fullheight">
-	      	<div class="overlay"></div>
-	        <div class="container-fluid p-0">
-	          <div class="row d-flex no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
-	          	<img class="one-third order-md-last img-fluid" src="{{ URL::asset('user/images/bg_2.png') }}" alt="">
-		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
-		          	<div class="text">
-		          		<span class="subheading">#New Arrival</span>
-		          		<div class="horizontal">
-				            <h1 class="mb-4 mt-3">New Shoes Winter Collection</h1>
-				            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
-				            
-				            <p><a href="#" class="btn-custom">Discover Now</a></p>
+		          		<div class="horizontal" >
+				            <h1 class="mb-4 mt-3" style="color: #fff;">BMS Online Registration</h1>
+				            <p class="mb-4" style="color: #fff;">Daftar event BMS secara praktis melalui BMS Online Registration.</p>
 				          </div>
 		            </div>
 		          </div>
